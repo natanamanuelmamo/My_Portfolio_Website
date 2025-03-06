22fe86935b98c28cb659b00bd3f9cc4e6746a57a
natanamanuelmamo <natanamanuelmamo@gmail.com>
2025-03-06 12:21:41 +0300

    finished main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,7 @@ function headerShadow() {
   }
 }
 
+
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
   strings : ["Frontend Developer","Backend Developer","AI Developer"],
@@ -100,3 +101,40 @@ srRight.reveal('.form-control',{delay: 100})
 srRight.reveal('.Work',{delay: 100})
 srRight.reveal('.alldev',{delay: 100})
 srRight.reveal('.right-slide',{delay: 100})
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+const scrollY = window.scrollY;
+
+sections.forEach(current =>{
+  const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute('id')
+
+  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+  }  else {
+
+    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+  }
+})
+}
+
+window.addEventListener('scroll', scrollActive)
+
+function downloadFile(Kirubel_s_Resume) {
+  const element = document.createElement('a');
+  element.href = Kirubel_s_Resume; 
+  element.download = Kirubel_s_Resume; 
+  document.body.appendChild(element); 
+  element.click(); 
+  document.body.removeChild(element); 
+}
